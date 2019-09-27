c7e1d6b20a46451a4d2adec5cb5781abe140cf33
Jussi Järvinen <jussi.jarvinen@outotec.com>
2019-09-27 09:07:36 +0300

    Updated position text max height as it was clipped in mobile

--- src/components/position.js
+++ src/components/position.js
@@ -27,7 +27,7 @@ const Description = styled.div`
   margin-left: 1.8rem;
   margin-right: 1.8rem;
   overflow: hidden;
-  max-height: 2000px;
+  max-height: 3000px;
   box-sizing: border-box;
   transition: max-height 0.5s ease;
   margin-top: -1rem;
